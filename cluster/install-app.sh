13a32ce54ab4b3ee43223b14ca07e13f13930835
Gijs van Dulmen <gijs@vandulmen.net>
2020-11-28 21:48:52 +0100

    Add better instructions and shutdown script

--- cluster/install-app.sh
+++ cluster/install-app.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 kubectl create ns application01
 kubectl apply -f ./app-infra
+
+echo "Use the following to login to ArgoCD: "
+echo "Domain:"
+echo " https://argocd.test02.ziny.nl/"
+echo "User:"
+echo " admin"
+echo "Password: "
+echo -n " "
+kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server -o name | cut -d'/' -f 2
