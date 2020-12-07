cb7583bf63cde19795a3717bfcf27c599b0d3eea
Gijs van Dulmen <gijs@vandulmen.net>
2020-12-07 21:04:18 +0100

    Add variables to destroy command

--- 3-shutdown.sh
+++ 3-shutdown.sh
@@ -2,4 +2,7 @@
 source 0-config.sh
 
 cd gke-cluster-01
-terraform destroy
+terraform destroy \
+    -var="project_id=${PROJECT_ID}" \
+    -var="cluster_name=${PROJECT_ID}" \
+    -var="region=${REGION}"
